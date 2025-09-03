cbe73f8c70bc3fca9a5c746830f0757d5e672b12
reme25241-cloud <reme25241@gmail.com>
2025-09-03 09:44:32 +0530

    Delete fleet directory

--- fleet/templatetags/fleet_extras.py
+++
@@ -1,9 +0,0 @@
-from django import template
-register = template.Library()
-
-@register.filter
-def get_item(d, key):
-    try:
-        return d.get(key, '')
-    except Exception:
-        return ''
